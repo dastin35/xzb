a334cd58bbc78210737b7f6c679130047d67909d
dastin35 <78873646+dastin35@users.noreply.github.com>
2021-02-10 22:37:13 +0300

    Add files via upload

---
+++ my_bot_xzb.py
@@ -0,0 +1,18 @@
+Python 3.8.7 (tags/v3.8.7:6503f05, Dec 21 2020, 17:59:51) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import telebot
+>>> bot = telebot.TeleBot('1690954290:AAHvr9RcGSggitFGTLKr4deIltf1Iyq_Hgg')
+>>> @bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, f'Я бот. Приятно познакомиться, {message.from_user.first_name}')
+
+    
+>>> @bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+
+        
+>>> bot.polling(none_stop=True)
